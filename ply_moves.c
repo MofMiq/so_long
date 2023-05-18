396c95aa4f2363b11fdc281062fcd2a17e063afe
Marina Alba Rodriguez Estebanez <117929211+MofMiq@users.noreply.github.com>
2023-05-18 12:06:49 +0200

    in progress
    adding comments

--- ply_moves.c
+++ ply_moves.c
@@ -12,10 +12,15 @@
 
 #include "so_long.h"
 
-/*esta funcion simplemente pilla la tecla del teclado que se esta presionando,
-como está siendo presionada y le asigna un movimiento dentro del mapa. Creamos
-la orden por decirlo asi, pero en si misma no hace ningun movimiento porque para
-eso hay que llamar a la funcion mlx_is_key_down*/
+/*This function simply captures the keyboard key that is being pressed and then
+calls another function that specifies how the movement will occur within the map
+(creating the corresponding command). Additionally, we assign a function to the
+ESC key that closes the game and the window.
+We calculate the player's position in the map through the instance, which is the
+image and we divide it by the size of the sprites.
+To ensure that the player moves one cell at a time instead of continuously, we
+create a boolean variable called 'is_pressed' within the 'game' structure that
+prevents excessive movements.*/
 
 void	ft_controls(void *param)
 {
@@ -24,8 +29,6 @@ void	ft_controls(void *param)
 	game = param;
 	game->curr_py = game->plyer_img->instances[0].y / 32;
 	game->curr_px = game->plyer_img->instances[0].x / 32;
-	//recalculamos l aposicion del jugador en el mapa atraves de la instancia de la imagen / el tamaño de los sprites
-	//ft_printf("curr_px: %d, curr_py: %d\n", game->curr_px, game->curr_py);
 	if (mlx_is_key_down(game->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(game->mlx);
 	if (game->is_pressed == 0)
@@ -43,6 +46,17 @@ void	ft_controls(void *param)
 		game->is_pressed = 0;
 }
 
+/*The 'up' movement is executed when we press 'W'. Here's how it is defined:
+If the player encounters a '1' (wall), the movement is terminated, and the
+character doesn't advance.
+If the player encounters a collectible, the ft_collectibles function is executed.
+For any other case, the following steps are performed:
+Move the image through the instance in the desired direction.
+Update the player's position on the map based on '1' and '0'.
+Increment the player's total movement count by one in 'moves'.
+Set the boolean variable is_pressed to 1 to prevent additional movements from
+being executed.*/
+
 void	ft_up(t_game *game)
 {
 	if (game->map[game->curr_py - 1][game->curr_px] == '1')
@@ -58,6 +72,8 @@ void	ft_up(t_game *game)
 	game->is_pressed = 1;
 	ft_printf("Moves: %d\n", game->moves);
 }
+
+//The 'down' movement is executed when we press 'S'. Idem.
 
 void	ft_down(t_game *game)
 {
@@ -75,6 +91,8 @@ void	ft_down(t_game *game)
 	ft_printf("Moves: %d\n", game->moves);
 }
 
+//The 'left' movement is executed when we press 'A'. Idem.
+
 void	ft_left(t_game *game)
 {
 	if (game->map[game->curr_py][game->curr_px - 1] == '1')
@@ -90,6 +108,8 @@ void	ft_left(t_game *game)
 	game->is_pressed = 1;
 	ft_printf("Moves: %d\n", game->moves);
 }
+
+//The 'right' movement is executed when we press 'D'. Idem.
 
 void	ft_right(t_game *game)
 {
